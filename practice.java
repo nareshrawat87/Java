42fdf973cf95659e438763dd7c6e1467c3672c4e
nareshrawat87 <nareshrawatchaudhary@gmail.com>
2025-07-30 14:45:10 +0530

    adding new file

---
+++ practice.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class practice {
+    public static void main(String[] args) {
+        int a=12;
+        int b=-20;
+        int c = a+b;
+        System.out.println(~a);
+        System.out.println(~b);
+    }
+    
+}
